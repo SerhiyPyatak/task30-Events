f9353828be79fbed388b6ec8ff36602f725ac218
unknown <C:\The Bat!>
2021-04-08 14:30:19 +0300

    This commit adds ESLint-fixed js file

--- js/script.js
+++ js/script.js
@@ -1,8 +1,8 @@
 const likeButton = document.getElementById('likes-btn');
 let likesCounter = 0;
 likeButton.addEventListener(
-    'click',
-    (event) => {
-        document.getElementById('likes-counter').innerHTML = ++likesCounter;
-    }
+  'click',
+  (event) => {
+    document.getElementById('likes-counter').innerHTML = ++likesCounter;
+  }
 )
