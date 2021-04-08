c9ae4d4250081a43dc14d9c193ab3a3ca0157ba0
unknown <C:\The Bat!>
2021-04-08 15:13:17 +0300

    This commit completes third task

--- js/script.js
+++ js/script.js
@@ -1,8 +1,24 @@
 const likeButton = document.getElementById('likes-btn');
+const prohibited = document.getElementById( 'prohibited' );
 let likesCounter = 0;
+
 likeButton.addEventListener(
   'click',
   (event) => {
     document.getElementById('likes-counter').innerHTML = ++likesCounter;
   }
+)
+
+prohibited.addEventListener(
+    'mousedown',
+    (event) => {
+        if(event.which == 3) {
+            console.log('Context menu called');
+            alert ('Context menu calling is prohibited!');
+        } else {
+            console.log('Selection start');
+            alert ('Selection inside this block is prohibited!');
+        }
+        return false;
+    }
 )
