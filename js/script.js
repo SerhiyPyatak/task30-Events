2ac868f43b37feaa0709326b3c6e2c999c22543b
SerhiyPyatak <serhiypyatak@yahoo.com>
2021-04-10 09:37:40 +0300

    This is fifth husky commit

--- js/script.js
+++ js/script.js
@@ -4,22 +4,22 @@ let likesCounter = 0;
 
 
 document.addEventListener('keydown', (event) => {
-	const {code, ctrlKey, key} = event;
-	event.preventDefault();
-	const txtArea = document.getElementById('task1-textarea');
-	const divElement = document.getElementById('whiteboard');
-	if (code == "KeyE" && ctrlKey) {
-		divElement.style.display = "none";
-		txtArea.style.display = "block";
-		txtArea.innerHTML = divElement.innerHTML;
-	} else if (code == "KeyS" && ctrlKey) {
-		divElement.style.display = "block";
-		txtArea.style.display = "none";
-		divElement.innerHTML = txtArea.innerHTML;
-	} else {
-		txtArea.innerHTML += key;
-		//console.log();
-	}
+  const { code, ctrlKey, key } = event;
+  event.preventDefault();
+  const txtArea = document.getElementById('task1-textarea');
+  const divElement = document.getElementById('whiteboard');
+  if(code == "KeyE" && ctrlKey) {
+    divElement.style.display = "none";
+    txtArea.style.display = "block";
+    txtArea.innerHTML = divElement.innerHTML;
+  } else if(code == "KeyS" && ctrlKey) {
+    divElement.style.display = "block";
+    txtArea.style.display = "none";
+    divElement.innerHTML = txtArea.innerHTML;
+  } else {
+    txtArea.innerHTML += key;
+    //console.log();
+  }
 })
 
 likeButton.addEventListener(
@@ -31,7 +31,7 @@ likeButton.addEventListener(
 
 prohibited.addEventListener(
   'mousedown',
-  (event) => {
+  (   event) => {
     if(event.which == 3) {
       console.log('Context menu called');
       alert ('Context menu calling is prohibited!');
