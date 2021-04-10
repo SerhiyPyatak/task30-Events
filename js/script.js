1855310f1c25c2028a94a30e5f75d0ea0887622a
SerhiyPyatak <serhiypyatak@yahoo.com>
2021-04-10 09:09:19 +0300

    This commit adds first task resolving

--- js/script.js
+++ js/script.js
@@ -2,6 +2,25 @@ const likeButton = document.getElementById('likes-btn');
 const prohibited = document.getElementById('prohibited');
 let likesCounter = 0;
 
+document.addEventListener('keydown', (event) => {
+	const {code, ctrlKey, key} = event;
+	event.preventDefault();
+	const txtArea = document.getElementById('task1-textarea');
+	const divElement = document.getElementById('whiteboard');
+	if (code == "KeyE" && ctrlKey) {
+		divElement.style.display = "none";
+		txtArea.style.display = "block";
+		txtArea.innerHTML = divElement.innerHTML;
+	} else if (code == "KeyS" && ctrlKey) {
+		divElement.style.display = "block";
+		txtArea.style.display = "none";
+		divElement.innerHTML = txtArea.innerHTML;
+	} else {
+		txtArea.innerHTML += key;
+		//console.log()
+	}
+})
+
 likeButton.addEventListener(
   'click',
   (event) => {
